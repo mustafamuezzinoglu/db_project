94132438569200d4873dcabdf282fe8b54f10f5c
Mustafa <mustafamuezzinoglu@gmail.com>
2023-03-31 18:26:56 +0300

    Update create_table.java

--- src/test/java/tasks/create_table.java
+++ src/test/java/tasks/create_table.java
@@ -3,13 +3,9 @@ package tasks;
 public class create_table {
     public static void main(String[] args) {
 
-        System.out.println("my first table");
+        System.out.println("my conflict");
+        
+        System.out.println("my conflict");
 
-        System.out.println("my second work");
-
-
-        System.out.println("created  a new branch");
-
-        System.out.println("my branch is ok branch");
-    }
+        
 }
