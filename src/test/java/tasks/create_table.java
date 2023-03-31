638948c3e42f7b61b3d21b1765411c9c1dad72c3
Mustafa <mustafamuezzinoglu@gmail.com>
2023-03-31 18:28:14 +0300

    conflict

--- src/test/java/tasks/create_table.java
+++ src/test/java/tasks/create_table.java
@@ -3,13 +3,11 @@ package tasks;
 public class create_table {
     public static void main(String[] args) {
 
-        System.out.println("my first table");
-
-        System.out.println("my second work");
 
 
-        System.out.println("created  a new branch");
+        System.out.println("is there any conflict");
 
-        System.out.println("my branch is ok branch");
+
+
     }
 }
